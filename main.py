7bbc2af48c63811e1009a4930b579206974a01bb
Klinsmanagam <klinsmanagam@gmail.com>
2023-04-13 03:28:48 -0700

    Update main.py
    Feel free to share your algorithm.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 class SummarizeArticle:
-  """Class docstrings go here."""
+  """Problem: The class that summarises an article where every word beigins with letter G chatgpt4 release minute 3 https://youtu.be/outcGtbnMuQ?t=191 """
+  """ 
+  Algorithm
+  
+  Start:
+  
+  0. Receive the article variable
+  1. Pick the variable
+  2. Convert it to a datatype
+  3. Loop through the data finding words that starts with letter G
+  4. Save these no. of words to another variable with the key:value
+  5. Detect the language of the words using NLTK package
+  6. Find sentences with a higher no. of occurance of words that start with g
+  7. The sentences are selected and form the summary.
+  8. Print the values of the summary
+  
+  End
+  
+  N/B: There will be functions within the class to perform this algorthims.
+  """
+  
